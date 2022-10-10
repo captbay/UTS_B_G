89bddb22ccb9e09bca2c5edb7322a401ea8fbc50
TheoDorus <theodorusadiyatma96@gmail.com>
2022-10-10 18:00:12 +0700

    revisi dikit(blm fix)

--- process/peminjamanProcess.php
+++ process/peminjamanProcess.php
@@ -8,8 +8,10 @@
         $id_buku = $_GET['id_buku'];
         $tanggal_pinjam = $_POST['tanggal_pinjam'];
         $tanggal_kembali = $_POST['tanggal_kembali'];
-        $status = $_POST['status'];
-        
+        $status = 'dipinjam';
+        $kurang = 1;
+        $jumlah_tersedia = $_SESSION['buku']['jumlah_tersedia'];
+        $jumlah = $jumlah_tersedia - $kurang;
         //$tgl=date('d-m-Y');
         
         $query = mysqli_prepare(
@@ -22,9 +24,13 @@
         mysqli_stmt_execute($query);
         // perintah mysql yang gagal dijalankan ditangani oleh perintah “or die”
         if ($query) {
-            $update = mysqli_prepare($con,"UPDATE buku SET jumlah_tersedia = jumlah_tersedia - 1 WHERE id_buku = ?");
+            $update = mysqli_prepare($con,"UPDATE buku SET jumlah_tersedia = ? WHERE id_buku = ?");
             mysqli_stmt_bind_param($query, 'ii',$jumlah, $id_buku);
             mysqli_stmt_execute($update);
+
+            $stat = mysqli_prepare($con,"UPDATE buku SET status = ? WHERE id_buku = ?");
+            mysqli_stmt_bind_param($query, 'si',$status, $id_buku);
+            mysqli_stmt_execute($stat);
             echo
             '<script>
         alert("Peminjaman Success");
