f7d54cb7ea6b1a0d4f20c2a1580dbb6438405eab
TheoDorus <theodorusadiyatma96@gmail.com>
2022-10-10 18:53:16 +0700

    revisi dikit(blm fix)

--- process/editRuanganProcess.php
+++ process/editRuanganProcess.php
@@ -3,32 +3,25 @@
     if(isset($_POST['addRuangan'])){
 
         include('../db.php');
-
+        
+        $id_ruangan = $_GET['id_ruangan'];
         $nama_ruangan = $_POST['nama_ruangan'];
         $kapasitas = $_POST['kapasitas'];
         $status = $_POST['status'];
         
-        //$tgl=date('d-m-Y');
-        
-        $query = mysqli_prepare(
-            $con,
-            "INSERT INTO ruangan(nama_ruangan, kapasitas, status)
-        VALUES (?, ?, ?);"
-        )
-            or die(mysqli_error($con)); // try-catching error
-        mysqli_stmt_bind_param($query, 'sis', $nama_ruangan, $kapasitas, $status);
-        mysqli_stmt_execute($query);
+        $query = mysqli_prepare($con,"UPDATE ruangan SET nama_ruangan=?, kapasitas=?, status=? WHERE id_ruangan = ?");
+       mysqli_stmt_bind_param($query, 'sisi',$nama_ruangan,$kapasitas,$status, $id_ruangan);
+       mysqli_stmt_execute($query);
         // perintah mysql yang gagal dijalankan ditangani oleh perintah “or die”
         if ($query) {
-            echo
             '<script>
-        alert("Peminjaman Success");
+        alert("Edit Success");
         window.location = "../page/dashboardPage.php"
         </script>';
         } else {
             echo
             '<script>
-        alert("Peminjaman Failed");
+        alert("Edit Failed");
         window.history.back()
         </script>';
         }
